37cb4c5bb196205a8134043f509435aed5bc58a7
lesliemw <l.marie1598@gmail.com>
2025-03-19 21:50:12 +0000

    update file

--- server.py
+++ server.py
@@ -6,3 +6,23 @@ app = Flask(__name__)
 @app.route("/")
 def my_home():
     return render_template("index.html")
+
+
+@app.route("/contact")
+def contact_me():
+    return render_template("contact.html")
+
+
+@app.route("/work,<int:work_id>")
+def my_work(work_id=None):
+    return render_template("work.html", work_id=work_id)
+
+
+@app.route("/works")
+def my_works():
+    return render_template("works.html")
+
+
+@app.route("/aboutMe")
+def about_me():
+    return render_template("about.html")
