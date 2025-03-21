9fa3ecaf5af1d8c4d1f958c6668d58a438e79044
lesliemw <l.marie1598@gmail.com>
2025-03-21 20:57:39 +0000

    update file

--- server.py
+++ server.py
@@ -1,18 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def my_home():
+    '''This function will render the home page'''
     return render_template("index.html")
 
 
 @app.route("/<string:page_name>")
 def html_page(page_name):
+    '''This function is dynamic and will render the html page of whichever page is requested'''
     return render_template(page_name)
 
 
 @app.route("/submit_form", methods=['POST', 'GET'])
 def submit_form():
-    return "form submitted"
+    '''This function will be called when the form is submitted'''
+    if request.method == 'POST':
+        data = request.form.to_dict()
+        print(data)
+        return redirect("/thankyou.html")
+    else:
+        return "something went wrong. Try again"
